ab7d25562306864832bfbd9b0ef5c3b4d398fdcf
dhardin <dhardin@ltu.edu>
2014-01-15 16:29:04 -0500

    Added plugin script, stylesheet, and demo page.

--- jTable/js/jTable.js
+++ jTable/js/jTable.js
@@ -11,17 +11,17 @@
 //  GitHub: https://github.com/dhardin/jTable
 
 
-(function ($) {
+var jTable = (function ($) {
     //----------------- END MODULE SCOPE VARIABLES ---------------
     var configMap = {
-        color_table: 'blue',
-        color_text_active: '#000',
-        color_text_inactive: '#aaa'
+        row_highlight_class: 'jTable-Row',
+        cell_edit_class: 'jTable-Edit'
     },
+    stateMap = { $container: null },
     jqueryMap = {},
-    setJqueryMap, onClickCell, onDoubleClickCell,
+    setJqueryMap, onClick, onDoubleClick,
     objectCreate, extendObject,
-    $.fn.jTable, initModule;
+    publicMethod, initModule;
     //------------------- BEGIN UTILITY METHODS ------------------
     // ** Utility function to set inheritance
     // Cross-browser method to inherit Object.create()
@@ -32,7 +32,9 @@
         obj.prototype = arg;
         return new obj;
     };
+
     Object.create = Object.create || objectCreate;
+
     // ** Utility function to extend an object
     extendObject = function (orig_obj, ext_obj) {
         var key_name;
@@ -44,13 +46,39 @@
     };
     //-------------------- END UTILITY METHODS -------------------
     //--------------------- BEGIN DOM METHODS --------------------
-
+    // Begin dom method /setJqueryMap/
+    setJqueryMap = function () {
+        var $container = stateMap.$container;
+        jqueryMap.$container = $container;
+    };
+    // end dom method /setJqueryMap/
     //---------------------- END DOM METHODS ---------------------
     //------------------- BEGIN EVENT HANDLERS -------------------
-    // example: onClickButton = ...
+    // Begin Event handler /onClick/
+    onClick = function (e) {
+        e.preventDefault();
+        var row_highlight_class = configMap.row_highlight_class;
+        jqueryMap.$container.find('.' + row_highlight_class).removeClass(row_highlight_class);
+        $(this).siblings().andSelf().addClass(row_highlight_class);
+    };
+    // End Event handler /onClick/
+
+    // Begin Event handler /onDoubleClick/
+    onDoubleClick = function (e) {
+        e.preventDefault();
+        var $td = $(this),
+        cell_edit_class = configMap.cell_edit_class,
+        $prevEditDiv = jqueryMap.$container.find('.' + cell_edit_class);
+
+
+        $prevEditDiv.parent().html($prevEditDiv.html());
+
+        $td.html('<div contenteditable class="' + cell_edit_class + '">' + $td.html() + '</div>');
+        $td.focus();
+    };
+    // End Event handler /onDoubleClick/
     //-------------------- END EVENT HANDLERS --------------------
-    //------------------- BEGIN PUBLIC METHODS -------------------
-    // Begin public method /jTable/
+    // Begin public method /configModule/
     // Purpose : Adjust configuration of allowed keys
     // Arguments : A map of settable keys and values
     // * color_name - color to use
@@ -59,10 +87,44 @@
     // Returns : true
     // Throws : none
     //
-    	publicMethod = $.fn[jTable] = $[jTable] = function (options, callback) {
-		var $this = this;
-		
-		options = options || {};
+    configModule = function (input_map) {
+        jTable.butil.setConfigMap({
+            input_map: input_map,
+            settable_map: configMap.settable_map,
+            config_map: configMap
+        });
+        return true;
+    };
+    // End public method /configModule/
+    // Begin public method /initModule/
+    // Purpose : Initializes module
+    // Arguments :
+    // * $container the jquery element used by this feature
+    // Returns : true
+    // Throws : nonaccidental
+    //
+    initModule = function ($container) {
+        if ($container[0].nodeName == "TABLE") {
+            stateMap.$container = $container;
+            setJqueryMap();
+
+            jqueryMap.$container.find("td")
+            .on('click', onClick);
+
+            jqueryMap.$container.find("td")
+            .on('dblclick', onDoubleClick);
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    // End public method /initModule/
+    // return public methods
+    return {
+        configModule: configModule,
+        initModule: initModule
     };
     //------------------- END PUBLIC METHODS ---------------------
 
