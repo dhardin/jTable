eaa9e515b86e65630f3c9a4285847b6d25ffc9e7
dhardin <dhardin@ltu.edu>
2014-01-16 17:50:35 -0500

    Added option to add a drop down to a cell when in edit mode.

--- jTable/js/jTable.js
+++ jTable/js/jTable.js
@@ -15,13 +15,19 @@ var jTable = (function ($) {
     //----------------- END MODULE SCOPE VARIABLES ---------------
     var configMap = {
         row_highlight_class: 'jTable-Row',
-        cell_edit_class: 'jTable-Edit'
+        cell_edit_class: 'jTable-Edit',
     },
+    settingsMap = {
+        dropDownCol: [],
+        dropDownVal: []
+    },
+    main_HTML = String(),
     stateMap = { $container: null },
     jqueryMap = {},
     setJqueryMap, onClick, onDoubleClick, onBlur,
     objectCreate, extendObject, addSelectEdit,
-    publicMethod, initModule;
+    addDropDown, addDropDownValues,addEditiableDiv,
+    getEditValue, initModule;
     //------------------- BEGIN UTILITY METHODS ------------------
     // ** Utility function to set inheritance
     // Cross-browser method to inherit Object.create()
@@ -51,10 +57,65 @@ var jTable = (function ($) {
         var $container = stateMap.$container;
         jqueryMap.$container = $container;
     };
+    // End dom method /setJqueryMap/
+    // Begin dom method /addDropDown/
+    addDropDown = function (columnNum, selectedValue){
+        var 
+            dropDown_HTML = String()
+                            + '<select class="'+ configMap.cell_edit_class +'">',
+            columns = settingsMap.dropDownCol,
+            colIndex = settingsMap.dropDownCol.indexOf(columnNum),
+            values = settingsMap.dropDownVal[colIndex]
+        ;
+        
+        if (jQuery.inArray(columnNum, columns) > -1)
+        {
+            dropDown_HTML += addDropDownValues(values, selectedValue);
+        }
+        dropDown_HTML += '</select>';
 
-    addSelectEdit = function () {
+        return dropDown_HTML;
     };
-    // end dom method /setJqueryMap/
+    // Begin dom method /addDropDownValues/
+    addDropDownValues = function (values, selectedValue) {
+        var
+            value, 
+            values_HTML = String()
+        ;
+        for (value in values){
+            values_HTML += '<option value"' + values[value] + '" ' + (selectedValue == values[value] ? 'selected="true"' : '') + '>' + values[value] + '</option>';
+        }
+        return values_HTML;
+    };
+    // End dom method /addDropDownValues/
+
+    // Begin dom method /addDropDownValues/
+    addEditiableDiv = function (content) {
+        return ('<div contenteditable class="' + configMap.cell_edit_class + '">' +  content + '</div>');
+    };
+    // End dom method /addDropDownValues/
+
+    // Begin dom method /getEditHTML/
+    getEditValue = function($container) {
+        var
+            nodeName = $container[0].nodeName,
+            cell_edit_class = configMap.cell_edit_class
+        ;
+        if ($container.hasClass(cell_edit_class))
+        {
+            switch (nodeName)
+            {
+                case 'DIV':
+                    return $container.html();
+                case 'SELECT':
+                    return $container.val();
+                default:
+                    return false;   
+            }
+        }
+        return false;
+    };
+    // End dom method /getEditHTML/
     //---------------------- END DOM METHODS ---------------------
     //------------------- BEGIN EVENT HANDLERS -------------------
     // Begin Event handler /onClick/
@@ -69,15 +130,17 @@ var jTable = (function ($) {
     // Begin Event handler /onDoubleClick/
     onDoubleClick = function (e) {
         e.preventDefault();
-        var $this = $(this),
-        cell_edit_class = configMap.cell_edit_class,
-        $prevEditDiv = jqueryMap.$container.find('.' + cell_edit_class);
+        var 
+            $this = $(this),
+            cell_edit_class = configMap.cell_edit_class,
+            $prevEditDiv = jqueryMap.$container.find('.' + cell_edit_class),
+            columns = settingsMap.dropDownCol
+        ;
 
         if ($prevEditDiv.length > 0) {
-            $prevEditDiv.parent().html($prevEditDiv.html());
+            $prevEditDiv.parent().html(getEditValue($prevEditDiv));
         }
-
-        $this.html('<div contenteditable class="' + cell_edit_class + '">' + $this.html() + '</div>');
+        $this.html(jQuery.inArray($this[0].cellIndex, columns) > -1 ? addDropDown($this[0].cellIndex, $this.text()) : addEditiableDiv($this.html())) ;
         $this.find('.' + cell_edit_class).focus();
        
     };
@@ -91,7 +154,7 @@ var jTable = (function ($) {
         $prevEditDiv = $this.find('.' + cell_edit_class);
 
         if ($prevEditDiv.length > 0) {
-            $this.html($prevEditDiv.html());
+            $this.html(getEditValue($prevEditDiv));
         }
 
     };
@@ -107,12 +170,12 @@ var jTable = (function ($) {
     // Returns : true
     // Throws : none
     //
-    configModule = function (input_map) {
-        jTable.butil.setConfigMap({
-            input_map: input_map,
-            settable_map: configMap.settable_map,
-            config_map: configMap
-        });
+    configModule = function (settings_map) {
+        for (setting in settings_map) {
+            if (settingsMap.hasOwnProperty(setting)) {
+             settingsMap[setting] = settings_map[setting];               
+            }
+        }
         return true;
     };
     // End public method /configModule/
@@ -127,6 +190,12 @@ var jTable = (function ($) {
         if ($container[0].nodeName == "TABLE") {
             stateMap.$container = $container;
             setJqueryMap();
+
+            configMap.main_HTML = $container.html();
+
+            console.log(configMap.main_HTML);
+
+            console.log(settingsMap);
 
             jqueryMap.$container.find("td")
             .on('click', onClick);
